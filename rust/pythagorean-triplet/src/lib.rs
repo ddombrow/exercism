6bb4b0394dd44a23a3695c11337bbe64646e48be
Daniel Dombrowsky <ddombrow@gmail.com>
2018-01-16 22:37:15 -0500

    unoptimized solution for pythagorean triplets.

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,17 @@
-pub fn find() -> Option<u32> {
-    unimplemented!();
-}
+pub fn find() -> Option<u32> {
+    let mut x = 0_u32; 
+    for a in 1..1000 {
+        for b in a..1000 {
+            for c in b..1000 {
+                if (a as u32).pow(2) + (b as u32).pow(2) == (c as u32).pow(2) {
+                    if (a+b+c) == 1000 {
+                       x = a * b * c;
+                       break;
+                    }
+                }
+            }
+        }
+    }
+
+    Some(x)
+}
